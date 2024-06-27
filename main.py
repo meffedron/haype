3e35c663f67822f78caf7183be76f7be0d6c6fe1
meffedron <vlad.mef@mail.ru>
2024-06-27 12:30:16 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-# Функция для проверки, являются ли строки анаграммами
+
 def are_anagrams(str1, str2):
     return sorted(str1) == sorted(str2)
 
-# Запрос ввода от пользователя
+
 user_str1 = input("Введите первую строку: ")
 user_str2 = input("Введите вторую строку: ")
 
-# Вызов функции и вывод результата
+
 if are_anagrams(user_str1, user_str2):
     print("Строки являются анаграммами.")
 else:
